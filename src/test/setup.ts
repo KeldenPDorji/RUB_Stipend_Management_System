c6854b6095e567fff92c9b287cbdca8fc3ca0dc9
KeldenPDorji <02230285.cst@rub.edu.bt>
2025-09-18 11:26:11 +0600

    😎

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,40 @@
 import '@testing-library/jest-dom'
+
+// Mock ResizeObserver
+global.ResizeObserver = class ResizeObserver {
+  constructor(cb: any) {
+    this.cb = cb;
+  }
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+  cb: any;
+};
+
+// Mock matchMedia
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: (query: string) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: () => {},
+    removeListener: () => {},
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => {},
+  }),
+});
+
+// Mock IntersectionObserver
+(global as any).IntersectionObserver = class IntersectionObserver {
+  root = null;
+  rootMargin = '';
+  thresholds = [];
+  
+  constructor() {}
+  observe() {}
+  disconnect() {}
+  unobserve() {}
+  takeRecords() { return []; }
+};
